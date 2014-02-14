2b47efc9b96fd02c4f34b64ae05f7626f6de36c5
James Roland <git@jamesroland.com>
2014-02-13 17:10:33 -0800

    Initial commit.  Project started.

---
+++ kafka-net/Common/Extensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace kafka_net.Common
+{
+    public static class Extensions
+    {
+        public static byte[] ToBytes(this string value)
+        {
+            return Encoding.UTF8.GetBytes(value).Reverse().ToArray();
+        }
+
+        public static byte[] ToBytes(this Int16 value)
+        {
+            return BitConverter.GetBytes(value).Reverse().ToArray();
+        }
+
+        public static byte[] ToBytes(this Int32 value)
+        {
+            return BitConverter.GetBytes(value).Reverse().ToArray();
+        }
+
+        public static byte[] ToBytes(this Int64 value)
+        {
+            return BitConverter.GetBytes(value).Reverse().ToArray();
+        }
+
+        public static byte[] ToBytes(this float value)
+        {
+            return BitConverter.GetBytes(value).Reverse().ToArray();
+        }
+
+        public static byte[] ToBytes(this double value)
+        {
+            return BitConverter.GetBytes(value).Reverse().ToArray();
+        }
+
+        public static byte[] ToBytes(this char value)
+        {
+            return BitConverter.GetBytes(value).Reverse().ToArray();
+        }
+
+        public static byte[] ToBytes(this bool value)
+        {
+            return BitConverter.GetBytes(value).Reverse().ToArray();
+        }
+
+
+        public static Int32 ToInt32(this byte[] value)
+        {
+            return BitConverter.ToInt32(value, 0);
+        }
+    }
+}
---
+++ kafka-net/KafkaConnection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using kafka_net.Common;
+
+namespace Kafka.Client
+{
+    public class KafkaConnection : IDisposable
+    {
+        private readonly Uri _kafkaUri;
+        private readonly int _readTimeoutMS;
+        private readonly TcpClient _client = new TcpClient();
+
+        /// <summary>
+        /// Initializes a new instance of the KafkaConnection class.
+        /// </summary>
+        /// <param name="serverAddress"></param>
+        /// <param name="readTimeoutMS">The timeout for read operations</param>
+        public KafkaConnection(Uri serverAddress, int readTimeoutMS = 30000)
+        {
+            _kafkaUri = serverAddress;
+            _readTimeoutMS = readTimeoutMS;
+        }
+
+        public Uri KafkaUri
+        {
+            get { return _kafkaUri; }
+        }
+
+        /// <summary>
+        /// Send payload to the kafka server
+        /// </summary>
+        /// <param name="payload">kafka protocol formatted byte[] payload</param>
+        /// <returns>Task handle to send operation.</returns>
+        public async Task SendAsync(byte[] payload)
+        {
+            var conn = await GetClient();
+            using (var stream = conn.GetStream())
+            {
+                await stream.WriteAsync(payload, 0, payload.Length);
+            }
+        }
+
+        /// <summary>
+        /// Read a response from kafka server
+        /// </summary>
+        /// <returns>Task handle with byte[] of response data from kafka.</returns>
+        public async Task<byte[]> ReadAsync()
+        {
+            //get message size from header
+            var header = await ReadAsync(4);
+
+            var size = header.ToInt32();
+
+            return await ReadAsync(size);
+        }
+
+        private async Task<byte[]> ReadAsync(int size)
+        {
+            var conn = await GetClient();
+            using (var stream = conn.GetStream())
+            {
+                stream.ReadTimeout = _readTimeoutMS;
+
+                var buffer = new byte[size];
+                await stream.ReadAsync(buffer, 0, size);
+                return buffer;
+            }
+        }
+
+        private async Task<TcpClient> GetClient()
+        {
+            if (_client.Connected == false)
+            {
+                await _client.ConnectAsync(_kafkaUri.Host, _kafkaUri.Port);
+            }
+            return _client;
+        }
+
+        public void Dispose()
+        {
+            using (_client)
+            {
+
+            }
+        }
+    }
+}
