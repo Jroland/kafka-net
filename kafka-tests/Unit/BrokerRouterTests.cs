cbd34fbb92e15d2fbdc29b69fa2abd982a17770c
James Roland <me@jamesroland.com>
2014-02-25 22:25:52 -0800

    Start build out of moq unit tests

---
+++ kafka-tests/Unit/BrokerRouterTests.cs
@@ -0,0 +1,38 @@
+﻿using KafkaNet;
+using Moq;
+using Ninject.MockingKernel.Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kafka_tests.Unit
+{
+    [TestFixture]
+    public class BrokerRouterTests
+    {
+        private MoqMockingKernel _kernel;
+
+        [SetUp]
+        public void Setup()
+        {
+            _kernel = new MoqMockingKernel();
+        }
+        
+        [Test]
+        public void BrokerRouterCanConstruct()
+        {
+            var conn = _kernel.GetMock<IKafkaConnection>();
+            var factory = _kernel.GetMock<IKafkaConnectionFactory>();
+            factory.Setup(x => x.Create(It.IsAny<Uri>(), It.IsAny<int>(), It.IsAny<IKafkaLog>())).Returns(() => conn.Object);
+
+            var result = new BrokerRouter(new KafkaNet.Model.KafkaOptions
+            {
+                KafkaServerUri = new List<Uri> { new Uri("http://localhost:9999") },
+                KafkaConnectionFactory = factory.Object
+            });
+        }
+    }
+}
