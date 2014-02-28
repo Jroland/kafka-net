d174b6d3c24250575acefc91fc011dde2951e81c
James Roland <git@jamesroland.com>
2014-02-28 14:24:13 -0800

    Clean TestHarness application.

--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using KafkaNet;
 using KafkaNet.Model;
@@ -21,11 +19,12 @@ namespace TestHarness
                     var consumer = new Consumer(new ConsumerOptions {Topic = "TestHarness", Router = router});
                     foreach (var data in consumer.Consume())
                     {
-                        Console.WriteLine("Response:{0}", data.Value);
+                        Console.WriteLine("Response: {0}", data.Value);
                     }
                 });
 
 
+            Console.WriteLine("Type a message and press enter...");
             while (true)
             {
                 var message = Console.ReadLine();
@@ -34,92 +33,5 @@ namespace TestHarness
             }
             
         }
-
-        //private static void SendOffsetCommitRequest(Producer client)
-        //{
-        //    var request = new OffsetCommitRequest()
-        //    {
-        //        CorrelationId = 1,
-        //        ConsumerGroup = "TestHarnessGroup",
-        //        OffsetCommits = new List<OffsetCommit>(new[]{
-        //            new OffsetCommit{
-        //                Topic = "TestHarness",
-        //                PartitionId = 0,
-        //                Offset = 6,
-        //                Metadata = "Test metadata"
-        //            }
-        //        })
-        //    };
-
-        //    var result = client.SendAsync(request).Result;
-        //}
-
-        //private static void SendOffsetRequest(Producer client)
-        //{
-        //    var request = new OffsetRequest
-        //    {
-        //        CorrelationId = 1,
-        //        Offsets = new List<Offset>(new[]
-        //                {
-        //                    new Offset
-        //                        {
-        //                            Topic = "TestHarness",
-        //                            PartitionId = 0,
-        //                            MaxOffsets = 1,
-        //                            Time = -1
-        //                        }
-        //                })
-        //    };
-
-        //    var result = client.SendAsync(request).Result;
-        //}
-
-        //private static void SendFetchRequest(Producer client)
-        //{
-        //    var request = new FetchRequest
-        //    {
-        //        CorrelationId = 1,
-        //        Fetches = new List<Fetch>(new[]
-        //                {
-        //                    new Fetch
-        //                        {
-        //                            Topic = "TestHarness",
-        //                            PartitionId = 0,
-        //                            Offset = 0
-        //                        }
-        //                })
-        //    };
-
-        //    var result = client.SendAsync(request).Result;
-        //}
-
-        //private static void SendMetadataRequest(Producer client)
-        //{
-        //    var request = new MetadataRequest()
-        //        {
-        //            Topics = new List<string>(new[] {"UnitTest"})
-        //        };
-
-        //    var result = client.SendAsync(request).Result;
-        //}
-
-        //private static void SendMessageTest(Producer client)
-        //{
-        //    var request = new ProduceRequest
-        //        {
-        //            ClientId = "kafka-python",
-        //            CorrelationId = 1,
-        //            Payload = new List<Payload>(new[]
-        //                {
-        //                    new Payload
-        //                        {
-        //                            Topic = "TestHarness",
-        //                            Messages = new List<Message>(new[] {new Message {Value = "Test Message"}})
-        //                        }
-        //                })
-        //        };
-
-        //    var result = client.SendAsync(request).Result;
-        //}
     }
 }
