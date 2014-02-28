9f0de312e7a06693141c869639be8b6e871a51be
James Roland <git@jamesroland.com>
2014-02-28 15:27:10 -0800

    Flush out sample Json Consumer/Producers

--- kafka-net-client/JsonConsumer.cs
+++ kafka-net-client/JsonConsumer.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using Newtonsoft.Json;
 
 namespace KafkaNet.Client
 {
-    public class JsonConsumer
+    public class JsonConsumer<T> : IDisposable
     {
+        private readonly ConsumerOptions _options;
+        private readonly Consumer _consumer;
 
+        public JsonConsumer(ConsumerOptions options)
+        {
+            _options = options;
+            _consumer = new Consumer(options);
+        }
+
+        public IEnumerable<Message<T>> Consume()
+        {
+            return _consumer.Consume()
+                .Select(response => new Message<T>
+                {
+                    Meta = response.Meta,
+                    Value = JsonConvert.DeserializeObject<T>(response.Value)
+                });
+        }
+
+        public void Dispose()
+        {
+            using (_consumer) { }
+        }
+    }
+
+    public class Message<T>
+    {
+        public MessageMetadata Meta { get; set; }
+        public T Value { get; set; }
     }
 }
--- kafka-net-client/JsonProducer.cs
+++ kafka-net-client/JsonProducer.cs
@@ -1,50 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using KafkaNet.Model;
 using KafkaNet.Protocol;
-using log4net;
 using Newtonsoft.Json;
 
 namespace KafkaNet.Client
 {
-    public class JsonProducer
+    public class JsonProducer : IDisposable
     {
-        private Producer _producer;
+        private readonly Producer _producer;
 
         public JsonProducer(KafkaOptions options)
         {
             _producer = new Producer(options);
         }
 
-        public Task<List<ProduceResponse>> Publish<T>(string topic, IEnumerable<T> messages,  Int16 acks = 1, int timeoutMS = 1000)
+        public Task<List<ProduceResponse>> Publish<T>(string topic, IEnumerable<T> messages, Int16 acks = 1, int timeoutMS = 1000) where T : class 
         {
             return _producer.SendMessageAsync(topic, ConvertToKafkaMessage(messages), acks, timeoutMS);
         }
 
-        private IEnumerable<Message> ConvertToKafkaMessage<T>(IEnumerable<T> messages)
+        private static IEnumerable<Message> ConvertToKafkaMessage<T>(IEnumerable<T> messages) where T : class 
         {
             var hasKey = typeof(T).GetProperty("Key", typeof(string)) != null;
 
-            return messages.Select(m =>
-            {
-                return new Message
-                    {
-                        Key = hasKey ? GetKeyPropertyValue(m) : null,
-                        Value = JsonConvert.SerializeObject(m)
-                    };
-            });
+            return messages.Select(m => new Message
+                {
+                    Key = hasKey ? GetKeyPropertyValue(m) : null,
+                    Value = JsonConvert.SerializeObject(m)
+                });
         }
 
-        private string GetKeyPropertyValue<T>(T message)
+        private static string GetKeyPropertyValue<T>(T message) where T : class 
         {
             if (message == null) return null;
             var info = message.GetType().GetProperty("Key", typeof(string));
 
             if (info == null) return null;
             return (string)info.GetValue(message);
+        }
+
+        public void Dispose()
+        {
+            using (_producer) { }
         }
     }
 }
