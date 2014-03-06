7a22aaf3948903ba0ddf28164393c6d9ed9dc973
James Roland <me@jamesroland.com>
2014-03-05 21:10:58 -0800

    Add unit tests

---
+++ kafka-tests/Unit/CommonQueriesTests.cs
@@ -0,0 +1,87 @@
+﻿using KafkaNet;
+using Moq;
+using Ninject.MockingKernel.Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kafka_tests.Unit
+{
+    [TestFixture]
+    public class CommonQueriesTest
+    {
+        private MoqMockingKernel _kernel;
+
+        [SetUp]
+        public void Setup()
+        {
+            _kernel = new MoqMockingKernel();
+        }
+
+        #region GetTopicOffset Tests...
+        [Test]
+        public void GetTopicOffsetShouldQueryEachBroker()
+        {
+            var routerProxy = new BrokerRouterProxy(_kernel);
+            var router = routerProxy.Create();
+            var common = new CommonQueries(router);
+
+            var result = common.GetTopicOffsetAsync(BrokerRouterProxy.TestTopic).Result;
+            Assert.That(routerProxy.BrokerConn0.OffsetRequestCallCount, Is.EqualTo(1));
+            Assert.That(routerProxy.BrokerConn1.OffsetRequestCallCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetTopicOffsetShouldThrowAnyException()
+        {
+            var routerProxy = new BrokerRouterProxy(_kernel);
+            routerProxy.BrokerConn0.OffsetResponseFunction = () => { throw new ApplicationException("test 99"); };
+            var router = routerProxy.Create();
+            var common = new CommonQueries(router);
+
+            common.GetTopicOffsetAsync(BrokerRouterProxy.TestTopic).ContinueWith(t =>
+            {
+                Assert.That(t.IsFaulted, Is.True);
+                Assert.That(t.Exception.Flatten().ToString(), Is.StringContaining("test 99"));
+            }).Wait();
+        } 
+        #endregion
+
+        #region GetTopic Tests...
+        [Test]
+        public void GetTopicShouldReturnTopic()
+        {
+            var routerProxy = new BrokerRouterProxy(_kernel);
+            var router = routerProxy.Create();
+            var common = new CommonQueries(router);
+
+            var result = common.GetTopic(BrokerRouterProxy.TestTopic);
+            Assert.That(result.Name, Is.EqualTo(BrokerRouterProxy.TestTopic));
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidTopicMetadataException))]
+        public void EmptyTopicMetadataShouldThrowException()
+        {
+            var routerProxy = new BrokerRouterProxy(_kernel);
+            var router = routerProxy.Create();
+            var common = new CommonQueries(router);
+
+            common.GetTopic("MissingTopic");
+        }
+
+        #endregion
+
+        [Test]
+        public void EnsureCommonQueriesDisposesRouter()
+        {
+            var router = _kernel.GetMock<IBrokerRouter>();
+            var common = new CommonQueries(router.Object);
+            using (common) { }
+            router.Verify(x => x.Dispose(), Times.Once());
+        }
+    }
+}
---
+++ kafka-tests/Unit/ConsumerTests.cs
@@ -0,0 +1,61 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using Moq;
+using Ninject.MockingKernel.Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kafka_tests.Unit
+{
+    [TestFixture]
+    public class ConsumerTests
+    {
+        private MoqMockingKernel _kernel;
+
+        [SetUp]
+        public void Setup()
+        {
+            _kernel = new MoqMockingKernel();
+        }
+
+        [Test]
+        public void ConsumerWhitelistShouldOnlyConsumeSpecifiedPartition()
+        {
+            var routerProxy = new BrokerRouterProxy(_kernel);
+            routerProxy.BrokerConn0.FetchResponseFunction = () => { return new FetchResponse(); };
+            var router = routerProxy.Create();
+            var options = CreateOptions(router);
+            options.PartitionWhitelist = new List<int> { 0 };
+            var consumer = new Consumer(options);
+            
+            var test = consumer.Consume().Take(1);
+            while (consumer.ConsumerTaskCount <= 0)
+            {
+                Thread.Sleep(100);
+            }
+
+            Assert.That(routerProxy.BrokerConn0.FetchRequestCallCount, Is.GreaterThanOrEqualTo(1));
+            Assert.That(routerProxy.BrokerConn1.FetchRequestCallCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void EnsureConsumerDisposesRouter()
+        {
+            var router = _kernel.GetMock<IBrokerRouter>();
+            var consumer = new Consumer(CreateOptions(router.Object));
+            using (consumer) { }
+            router.Verify(x => x.Dispose(), Times.Once());
+        }
+
+        private ConsumerOptions CreateOptions(IBrokerRouter router)
+        {
+            return new ConsumerOptions { Router = router, Topic = BrokerRouterProxy.TestTopic };
+        }
+    }
+}
