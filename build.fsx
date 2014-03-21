a97e5ac74afa72e64cb01c3aff9835f1ec1c455c
James Roland <me@jamesroland.com>
2014-03-20 20:35:03 -0700

    Add version increment with FAKE

--- build.fsx
+++ build.fsx
@@ -1,5 +1,6 @@
 #r ".tools/FAKE/tools/FakeLib.dll"
 open Fake
+open Fake.AssemblyInfoFile
 
 //download all nuget dependencies
 RestorePackages()
@@ -10,13 +11,31 @@ let deployDir = "./.deploy/"
 
 let kafkaNetSource = !! "src/kafka-net/*.csproj"
 
-let version = ""
+//version build increment
+let v = SemVerHelper.parse(System.IO.File.ReadAllText("version"))
+let buildNumber = System.Convert.ToInt32(v.Build) + 1
+let buildVersion = [v.Major; v.Minor; v.Patch; buildNumber;] |> Seq.map System.Convert.ToString |>  String.concat(".")
+
+Target "Record-Version-Increment" (fun _ ->
+    trace ("New build number is: " + buildVersion)
+    System.IO.File.WriteAllText("version", buildVersion)
+)
 
 Target "Clean" (fun _ -> 
     CleanDir buildDir
 )
 
 Target "Build-Kafka-Net" (fun _ ->
+    CreateCSharpAssemblyInfo "./src/kafka-net/properties/assemblyinfo.cs"
+        [Attribute.Title "kafka-net"
+         Attribute.Description "Native C# client for Apache Kafka."
+         Attribute.Guid "eb234ec0-d838-4abd-9224-479ca06f969d"
+         Attribute.Product "kafka-net"
+         Attribute.Company "James Roland"
+         Attribute.Copyright "Copyright James Roland 2014"
+         Attribute.Version buildVersion
+         Attribute.FileVersion buildVersion]
+         
     MSBuildRelease buildDir "Build" kafkaNetSource
      |> Log "AppBuild-Output: "
 )
@@ -50,10 +69,11 @@ Target "Default" (fun _ ->
 )
 
 "Clean"
+  ==> "Record-Version-Increment"
   ==> "Build-Kafka-Net"
   ==> "Build-Kafka-Tests"
-  =?> ("Run-Integration-Tests",hasBuildParam "Integration")
-  ==> "Run-Unit-Tests"
+  =?> ("Run-Integration-Tests", hasBuildParam "Integration")
+  =?> ("Run-Unit-Tests", hasBuildParam "Unit")
   ==> "Default"
  
 
