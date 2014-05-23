761f4e95072628207eec558ae4028990371ad60c
James Roland <me@jamesroland.com>
2014-05-22 22:16:11 -0700

    Code cleanup

--- src/kafka-net/Interfaces/IKafkaConnectionFactory.cs
+++ src/kafka-net/Interfaces/IKafkaConnectionFactory.cs
@@ -6,25 +6,8 @@ using System.Text;
 
 namespace KafkaNet
 {
-    public static class KafkaNetConfiguration
-    {
-        public static Func<Uri, int, IKafkaLog, IKafkaConnection> ConnectionFactory = (uri, timeout, log) => new KafkaConnection(new TcpSocket(uri), timeout, log);
-        public static Func<Topic, string, Partition> PartitionSelector = (topic, key) => new DefaultPartitionSelector().Select(topic, key);
-        public static Func<IKafkaLog> Log = () => new DefaultTraceLog();
-        public static Func<MessageCodec, byte[], byte[]> CompressFunction ;
-        public static Func<MessageCodec, byte[], byte[]> DeCompressFunction;
-    }
-
     public interface IKafkaConnectionFactory
     {
         IKafkaConnection Create(Uri kafkaAddress, int responseTimeoutMs, IKafkaLog log);
-    }
-
-    public class DefaultKafkaConnectionFactory : IKafkaConnectionFactory
-    {
-        public IKafkaConnection Create(Uri kafkaAddress, int responseTimeoutMs, IKafkaLog log)
-        {
-            return new KafkaConnection(new TcpSocket(kafkaAddress), responseTimeoutMs, log);
-        }
     }
 }
