0533b57785308456f89e1fd5fd7a867c9b5041d3
James Roland <me@jamesroland.com>
2014-05-22 21:52:46 -0700

    Flush out TcpSocket interface

--- src/kafka-net/Interfaces/ITcpSocket.cs
+++ src/kafka-net/Interfaces/ITcpSocket.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace KafkaNet.Interfaces
+namespace KafkaNet
 {
     public interface ITcpSocket
     {
-        void Connect(Uri server, int port);
-        
         Task<byte[]> ReadAsync(int readSize);
+        Task<byte[]> ReadAsync(int readSize, CancellationToken cancellationToken);
+
+        Task WriteAsync(byte[] buffer, int offset, int count);
+        Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken);
     }
 }
---
+++ src/kafka-net/TcpSocket.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using KafkaNet.Common;
+
+namespace KafkaNet
+{
+    public class TcpSocket : ITcpSocket
+    {
+        private readonly object _threadLock = new object();
+        private TcpClient _client;
+        private string _server;
+        private int _port;
+
+        public TcpSocket(string server, int port)
+        {
+            _server = server;
+            _port = port;
+        }
+
+        public Task<byte[]> ReadAsync(int readSize)
+        {
+            return GetClient().GetStream().ReadAsync(readSize);
+        }
+        public Task<byte[]> ReadAsync(int readSize, System.Threading.CancellationToken cancellationToken)
+        {
+            return GetClient().GetStream().ReadAsync(readSize, cancellationToken);
+        }
+
+        public Task WriteAsync(byte[] buffer, int offset, int count)
+        {
+            return GetClient().GetStream().WriteAsync(buffer, offset, count);
+        }
+        public Task WriteAsync(byte[] buffer, int offset, int count, System.Threading.CancellationToken cancellationToken)
+        {
+            return GetClient().GetStream().WriteAsync(buffer, offset, count, cancellationToken);
+        }
+
+        private TcpClient GetClient()
+        {
+            if (_client == null || _client.Connected == false)
+            {
+                lock (_threadLock)
+                {
+                    if (_client == null || _client.Connected == false)
+                    {
+                        _client = new TcpClient();
+                        _client.Connect(_server, _port);
+                    }
+                }
+            }
+            return _client;
+        }
+
+
+
+
+       
+
+       
+    }
+}
