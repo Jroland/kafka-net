b0918ecb2d3518c790fd2114c248dba32f2921d3
James Roland <git@jamesroland.com>
2014-06-03 20:28:13 -0700

    Fix integration tests all passing.
    Also updated the name to reflect integration in the class name.

--- src/kafka-tests/Integration/KafkaConnectionIntegrationTests.cs
+++ src/kafka-tests/Integration/KafkaConnectionIntegrationTests.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using KafkaNet;
 using KafkaNet.Model;
 using KafkaNet.Protocol;
 using NUnit.Framework;
+using kafka_tests.Helpers;
 
 namespace kafka_tests.Integration
 {
@@ -15,25 +14,18 @@ namespace kafka_tests.Integration
     /// </summary>
     [TestFixture]
     [Category("Integration")]
-    public class KafkaConnectionTests
+    public class KafkaConnectionIntegrationTests
     {
         private KafkaConnection _conn;
 
         [SetUp]
         public void Setup()
         {
-            var options = new KafkaOptions(new Uri(ConfigurationManager.AppSettings["IntegrationKafkaServerUrl"]));
+            var options = new KafkaOptions(IntegrationConfig.IntegrationUri);
             
             _conn = new KafkaConnection(new KafkaTcpSocket(new DefaultTraceLog(), options.KafkaServerUri.First()), options.ResponseTimeoutMs, options.Log);
         }
-
-        [Test]
-        [Ignore]
-        public void EnsureRequestCanRecoverFromDisconnection()
-        {
-
-        }
-
+        
         [Test]
         public void EnsureTwoRequestsCanCallOneAfterAnother()
         {
@@ -59,7 +51,7 @@ namespace kafka_tests.Integration
         public void EnsureMultipleAsyncRequestsCanReadResponses()
         {
             var requests = new List<Task<List<MetadataResponse>>>();
-            var singleResult = _conn.SendAsync(new MetadataRequest()).Result;
+            var singleResult = _conn.SendAsync(new MetadataRequest { Topics = new List<string>(new[] { IntegrationConfig.IntegrationTopic }) }).Result;
             Assert.That(singleResult.Count, Is.GreaterThan(0));
             Assert.That(singleResult.First().Topics.Count, Is.GreaterThan(0));
 
@@ -75,32 +67,30 @@ namespace kafka_tests.Integration
         [Test]
         public void EnsureDifferentTypesOfResponsesCanBeReadAsync()
         {
-            const string ExpectedTopic = "UnitTest";
-
             //just ensure the topic exists for this test
-            var ensureTopic = _conn.SendAsync(new MetadataRequest { Topics = new List<string>(new[] { ExpectedTopic }) }).Result;
+            var ensureTopic = _conn.SendAsync(new MetadataRequest { Topics = new List<string>(new[] { IntegrationConfig.IntegrationTopic }) }).Result;
 
             Assert.That(ensureTopic.Count, Is.GreaterThan(0));
             Assert.That(ensureTopic.First().Topics.Count, Is.EqualTo(1));
-            Assert.That(ensureTopic.First().Topics.First().Name == ExpectedTopic, Is.True, "ProduceRequest did not return expected topic.");
-            
+            Assert.That(ensureTopic.First().Topics.First().Name == IntegrationConfig.IntegrationTopic, Is.True, "ProduceRequest did not return expected topic.");
 
-            var result1 = _conn.SendAsync(RequestFactory.CreateProduceRequest(ExpectedTopic, "test"));
+
+            var result1 = _conn.SendAsync(RequestFactory.CreateProduceRequest(IntegrationConfig.IntegrationTopic, "test"));
             var result2 = _conn.SendAsync(new MetadataRequest());
-            var result3 = _conn.SendAsync(RequestFactory.CreateOffsetRequest(ExpectedTopic));
-            var result4 = _conn.SendAsync(RequestFactory.CreateFetchRequest(ExpectedTopic, 0));
+            var result3 = _conn.SendAsync(RequestFactory.CreateOffsetRequest(IntegrationConfig.IntegrationTopic));
+            var result4 = _conn.SendAsync(RequestFactory.CreateFetchRequest(IntegrationConfig.IntegrationTopic, 0));
 
             Assert.That(result1.Result.Count, Is.EqualTo(1));
-            Assert.That(result1.Result.First().Topic == ExpectedTopic, Is.True, "ProduceRequest did not return expected topic.");
+            Assert.That(result1.Result.First().Topic == IntegrationConfig.IntegrationTopic, Is.True, "ProduceRequest did not return expected topic.");
 
             Assert.That(result2.Result.Count, Is.GreaterThan(0));
-            Assert.That(result2.Result.First().Topics.Any(x => x.Name == ExpectedTopic), Is.True, "MetadataRequest did not return expected topic.");
+            Assert.That(result2.Result.First().Topics.Any(x => x.Name == IntegrationConfig.IntegrationTopic), Is.True, "MetadataRequest did not return expected topic.");
 
             Assert.That(result3.Result.Count, Is.EqualTo(1));
-            Assert.That(result3.Result.First().Topic == ExpectedTopic, Is.True, "OffsetRequest did not return expected topic.");
+            Assert.That(result3.Result.First().Topic == IntegrationConfig.IntegrationTopic, Is.True, "OffsetRequest did not return expected topic.");
 
             Assert.That(result4.Result.Count, Is.EqualTo(1));
-            Assert.That(result4.Result.First().Topic == ExpectedTopic, Is.True, "FetchRequest did not return expected topic.");
+            Assert.That(result4.Result.First().Topic == IntegrationConfig.IntegrationTopic, Is.True, "FetchRequest did not return expected topic.");
 
         }
     }
