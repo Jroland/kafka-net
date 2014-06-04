9a024a632321e2c81772dcc57bcab82ef4a54f01
James Roland <git@jamesroland.com>
2014-06-03 20:30:21 -0700

    Fix test for GZip integration.  All working and passing.

--- src/kafka-tests/Integration/GzipProducerConsumerTests.cs
+++ src/kafka-tests/Integration/GzipProducerConsumerTests.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using System.Linq;
 using KafkaNet;
 using KafkaNet.Model;
 using KafkaNet.Protocol;
 using NUnit.Framework;
+using kafka_tests.Helpers;
 
 namespace kafka_tests.Integration
 {
@@ -14,21 +13,23 @@ namespace kafka_tests.Integration
     public class GzipProducerConsumerTests
     {
         private BrokerRouter _router;
-        private const string CompressTopic = "CompressTest";
+        private KafkaConnection _kafkaConnection;
 
         [SetUp]
         public void Setup()
         {
-            var options = new KafkaOptions(new Uri(ConfigurationManager.AppSettings["IntegrationKafkaServerUrl"]));
-
+            var options = new KafkaOptions(IntegrationConfig.IntegrationUri);
+            _kafkaConnection = new KafkaConnection(new KafkaTcpSocket(new DefaultTraceLog(), options.KafkaServerUri.First()), options.ResponseTimeoutMs, options.Log);
             _router = new BrokerRouter(options);
         }
 
         [Test]
-        [Ignore]
         public void EnsureGzipCompressedMessageCanSend()
         {
-            var conn = _router.SelectBrokerRoute(CompressTopic, 0);
+            //ensure topic exists
+            _kafkaConnection.SendAsync(new MetadataRequest { Topics = new List<string>(new[] { IntegrationConfig.IntegrationCompressionTopic }) }).Wait();
+
+            var conn = _router.SelectBrokerRoute(IntegrationConfig.IntegrationCompressionTopic, 0);
 
             var request = new ProduceRequest
             {
@@ -39,7 +40,7 @@ namespace kafka_tests.Integration
                                     new Payload
                                         {
                                             Codec = MessageCodec.CodecGzip,
-                                            Topic = CompressTopic,
+                                            Topic = IntegrationConfig.IntegrationCompressionTopic,
                                             Partition = 0,
                                             Messages = new List<Message>
                                                     {
@@ -53,38 +54,16 @@ namespace kafka_tests.Integration
 
             var response = conn.Connection.SendAsync(request).Result;
             Assert.That(response.First().Error, Is.EqualTo(0));
-
-            //var offsets = producer.GetTopicOffsetAsync("NewTopic").Result;
-
-            //var consumer = new Consumer(new ConsumerOptions("NewTopic", _router),
-            //    offsets.Select(x => new OffsetPosition(x.PartitionId, x.Offsets.Max())).ToArray());
-
-            //var response = producer.SendMessageAsync("NewTopic", new[]
-            //    {
-            //        new Message {Value = "0", Key = "1"},
-            //        new Message {Value = "1", Key = "1"},
-            //        new Message {Value = "2", Key = "1"}
-            //    }, codec: MessageCodec.CodecGzip).Result;
-
-            //Assert.That(response.First().Error, Is.EqualTo(0));
-
-            //var results = consumer.Consume().Take(3).ToList();
-
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    Assert.That(results[i].Value, Is.EqualTo(i.ToString()));
-            //}
         }
 
         [Test]
-        [Ignore]
         public void EnsureGzipCanDecompressMessageFromKafka()
         {
             var producer = new Producer(_router);
 
-            var offsets = producer.GetTopicOffsetAsync(CompressTopic).Result;
+            var offsets = producer.GetTopicOffsetAsync(IntegrationConfig.IntegrationCompressionTopic).Result;
 
-            var consumer = new Consumer(new ConsumerOptions("Empty", _router),
+            var consumer = new Consumer(new ConsumerOptions(IntegrationConfig.IntegrationCompressionTopic, _router),
                 offsets.Select(x => new OffsetPosition(x.PartitionId, 0)).ToArray());
             
             var results = consumer.Consume().Take(3).ToList();
