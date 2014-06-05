add58d572350c44515972f06fd0b10cad385e939
James Roland <git@jamesroland.com>
2014-06-04 21:03:08 -0700

    Add equality override to OffsetRequest
    This was mainly to make unit tests easier.  Also override the ToString,
    again making unit test more clear when a failure occurs.

--- src/kafka-net/Protocol/OffsetRequest.cs
+++ src/kafka-net/Protocol/OffsetRequest.cs
@@ -118,5 +118,31 @@ namespace KafkaNet.Protocol
         }
         public int PartitionId { get; set; }
         public long Offset { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("PartitionId:{0}, Offset:{1}", PartitionId, Offset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((OffsetPosition)obj);
+        }
+
+        protected bool Equals(OffsetPosition other)
+        {
+            return PartitionId == other.PartitionId && Offset == other.Offset;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (PartitionId * 397) ^ Offset.GetHashCode();
+            }
+        }
     }
 }
