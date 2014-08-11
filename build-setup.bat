b24585d295777d888f613da93876c485f28d3243
James Roland <me@jamesroland.com>
2014-08-10 22:43:21 -0700

    House keeping remove old build scripts Replaced by appveyor

--- build-setup.bat
+++
@@ -1,5 +0,0 @@
-cls
-"nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" ".tools" "-ExcludeVersion"
-"nuget\nuget.exe" "install" "nunit.runners" "-OutputDirectory" ".tools" "-ExcludeVersion"
-"nuget\nuget.exe" "install" "nunit" "-OutputDirectory" ".tools" "-ExcludeVersion"
-pause
