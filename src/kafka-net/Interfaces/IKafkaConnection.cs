07d1a65308a684723fd43da31705d317e3c3e062
James Roland <git@jamesroland.com>
2014-09-23 15:34:00 -0700

    Update documentation

--- src/kafka-net/Interfaces/IKafkaConnection.cs
+++ src/kafka-net/Interfaces/IKafkaConnection.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using KafkaNet.Model;
 
 namespace KafkaNet
 {
     public interface IKafkaConnection : IDisposable
     {
+        /// <summary>
+        /// The unique endpoint location of this connection.
+        /// </summary>
         KafkaEndpoint Endpoint { get; }
+
+        /// <summary>
+        /// Value indicating the read polling thread is still active.
+        /// </summary>
         bool ReadPolling { get; }
-        System.Threading.Tasks.Task SendAsync(byte[] payload);
-        System.Threading.Tasks.Task<System.Collections.Generic.List<T>> SendAsync<T>(IKafkaRequest<T> request);
+
+        /// <summary>
+        /// Send raw payload data up to the connected endpoint.
+        /// </summary>
+        /// <param name="payload">The raw data to send to the connected endpoint.</param>
+        /// <returns>Task representing the future success or failure of query.</returns>
+        Task SendAsync(byte[] payload);
+
+        /// <summary>
+        /// Send a specific IKafkaRequest to the connected endpoint.
+        /// </summary>
+        /// <typeparam name="T">The type of the KafkaResponse expected from the request being sent.</typeparam>
+        /// <param name="request">The KafkaRequest to send to the connected endpoint.</param>
+        /// <returns>Task representing the future responses from the sent request.</returns>
+        Task<List<T>> SendAsync<T>(IKafkaRequest<T> request);
     }
 }
--- src/kafka-net/Interfaces/IPartitionSelector.cs
+++ src/kafka-net/Interfaces/IPartitionSelector.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
-using KafkaNet.Model;
-using KafkaNet.Protocol;
+﻿using KafkaNet.Protocol;
 
 namespace KafkaNet
 {
     public interface IPartitionSelector
     {
+        /// <summary>
+        /// Select the appropriate partition post a message based on topic and key data.
+        /// </summary>
+        /// <param name="topic">The topic at which the message will be sent.</param>
+        /// <param name="key">The data used to consistently route a message to a particular partition.  Value can be null.</param>
+        /// <returns>The partition to send the message to.</returns>
         Partition Select(Topic topic, byte[] key);
     }
 }
