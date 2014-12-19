4c5031a31b105dac0f5977556c5fad8418e8fd96
James Roland <me@jamesroland.com>
2014-12-18 16:10:25 -0800

    Update TestHarness example program

--- src/TestHarness/Program.cs
+++ src/TestHarness/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using KafkaNet;
+using KafkaNet.Common;
 using KafkaNet.Model;
 using KafkaNet.Protocol;
-using System.Collections.Generic;
 
 namespace TestHarness
 {
@@ -11,22 +11,24 @@ namespace TestHarness
     {
         static void Main(string[] args)
         {
+            //create an options file that sets up driver preferences
             var options = new KafkaOptions(new Uri("http://CSDKAFKA01:9092"), new Uri("http://CSDKAFKA02:9092"))
-                {
-                    Log = new ConsoleLog()
-                };
-            
-            var producer = new Producer(new BrokerRouter(options));
+            {
+                Log = new ConsoleLog()
+            };
 
+            //start an out of process thread that runs a consumer that will write all received messages to the console
             Task.Factory.StartNew(() =>
+            {
+                var consumer = new Consumer(new ConsumerOptions("TestHarness", new BrokerRouter(options)));
+                foreach (var data in consumer.Consume())
                 {
-                    var consumer = new Consumer(new ConsumerOptions("TestHarness", new BrokerRouter(options)));
-                    foreach (var data in consumer.Consume())
-                    {
-                        Console.WriteLine("Response: P{0},O{1} : {2}", data.Meta.PartitionId, data.Meta.Offset, data.Value);
-                    }
-                });
+                    Console.WriteLine("Response: P{0},O{1} : {2}", data.Meta.PartitionId, data.Meta.Offset, data.Value.ToUTF8String());
+                }
+            });
 
+            //create a producer to send messages with
+            var producer = new Producer(new BrokerRouter(options));
 
             Console.WriteLine("Type a message and press enter...");
             while (true)
@@ -35,6 +37,7 @@ namespace TestHarness
                 if (message == "quit") break;
                 if (string.IsNullOrEmpty(message))
                 {
+                    //special case, send multi messages quickly
                     for (int i = 0; i < 20; i++)
                     {
                         producer.SendMessageAsync("TestHarness", new[] { new Message(i.ToString()) })
@@ -48,7 +51,6 @@ namespace TestHarness
                 {
                     producer.SendMessageAsync("TestHarness", new[] { new Message(message) });
                 }
-
             }
 
             using (producer)
