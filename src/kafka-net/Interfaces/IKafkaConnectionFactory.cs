47fcef42341037bfd6a2d1e583f36afc371ccd85
Xharze <kimworking@gmail.com>
2015-03-23 13:31:37 +0100

    Add reconnection timeout to connection factory

--- src/kafka-net/Interfaces/IKafkaConnectionFactory.cs
+++ src/kafka-net/Interfaces/IKafkaConnectionFactory.cs
@@ -11,8 +11,9 @@ namespace KafkaNet
         /// <param name="endpoint">The specific KafkaEndpoint of the server to connect to.</param>
         /// <param name="responseTimeoutMs">The amount of time to wait for a message response to be received after sending a message to Kafka</param>
         /// <param name="log">Logging interface used to record any log messages created by the connection.</param>
+        /// <param name="maximumReconnectionTimeout"></param>
         /// <returns>IKafkaConnection initialized to connecto to the given endpoint.</returns>
-        IKafkaConnection Create(KafkaEndpoint endpoint, TimeSpan responseTimeoutMs, IKafkaLog log);
+        IKafkaConnection Create(KafkaEndpoint endpoint, TimeSpan responseTimeoutMs, IKafkaLog log, int? maximumReconnectionTimeout = null);
 
         /// <summary>
         /// Resolves a generic Uri into a uniquely identifiable KafkaEndpoint.
