7614cdc7491d0fc934865468cdc129c6852c3304
James Roland <me@jamesroland.com>
2015-03-31 11:47:03 -0700

    Add consolelog class for easier debugging

---
+++ src/kafka-net/Default/ConsoleLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace KafkaNet
+{
+    /// <summary>
+    /// This class simply logs all information out to the console. Usefull for 
+    /// debug testing in console applications.
+    /// </summary>
+    public class ConsoleLog : IKafkaLog
+    {
+        public void DebugFormat(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+
+        public void FatalFormat(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+    }
+}
