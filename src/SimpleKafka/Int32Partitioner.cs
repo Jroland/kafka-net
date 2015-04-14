37a736b59fb64f21f82608e2316ee7f4abfc05f4
Nick Randell <nick@nickrandell.com>
2015-04-14 16:02:07 +0100

    Tests for multiple partitions

---
+++ src/SimpleKafka/Int32Partitioner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleKafka
+{
+    public class Int32Partitioner : IKafkaMessagePartitioner<int>
+    {
+        public int CalculatePartition(int partitionKey, int numberOfPartitions)
+        {
+            return partitionKey % numberOfPartitions;
+        }
+    }
+}
