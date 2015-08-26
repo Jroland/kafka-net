659ed58816b76616348675d58d1257002627aa72
erano.of <eran.of@gigya-inc.com>
2015-08-26 17:40:25 +0300

    modify nuspec

--- src/KafkaNetClient/Properties/AssemblyInfo.cs
+++ src/KafkaNetClient/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("kafka-net")]
+[assembly: AssemblyTitle("KafkaNetClient")]
 [assembly: AssemblyDescription("Native C# client for Apache Kafka.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gigya Inc")]
-[assembly: AssemblyProduct("kafka-net")]
+[assembly: AssemblyProduct("KafkaNetClient")]
 [assembly: AssemblyCopyright("Copyright ©Gigya Inc 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: GuidAttribute("eb234ec0-d838-4abd-9224-479ca06f969d")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
